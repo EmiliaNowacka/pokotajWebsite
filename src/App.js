228888a4b2801652d331915fc031fe61f3567554
emilia <emiliamnowacka@gmail.com>
2023-06-30 17:17:46 +0200

    adding resposivnes and borders for debug

--- src/App.js
+++ src/App.js
@@ -19,19 +19,36 @@ function App() {
         <h1 className='Mission'>Naszą misją jest kompleksowa pomoc bezdomnym kotom na terenie Poznania i okolic</h1>
         <h1 className='OurCats'> Poznaj nasze koty </h1>
         <div className='CatGallery'>
-        <p className='MomoName'>Momo</p>
-        <p className='ClickMomo'>kliknij na mnie!</p>
-        <a href={links.momo_link} target="_blank" rel="noreferrer"><img src={momo} className="Momo" alt="logo" /></a>
-        <p className='HenryName'>Henry</p>
-        <p className='ClickHenry'>kliknij na mnie!</p>
-        <a href={links.henry_link} target="_blank" rel="noreferrer"><img src={henry2} className="Henry"/></a>
-        <p className='InkaName'>Inka</p>
-        <p className='ClickInka'>kliknij na mnie!</p>
-        <a href={links.inka_link} target="_blank" rel="noreferrer"><img src={inka} className="Inka"/></a>
-        <p className='PikotkaName'>Pikotka</p>
-        <p className='ClickPikotka'>kliknij na mnie!</p>
-        <a href={links.pikotka_link} target="_blank" rel="noreferrer"><img src={pikotka} className="Pikotka"/></a>
+          <div className='MomoContainer'>
+            <p className='MomoName'>Momo</p>
+            <p className='ClickMomo'>kliknij na mnie!</p>
+            <a href={links.momo_link} target="_blank" rel="noreferrer"><img src={momo} className="Momo" alt="logo" /></a>
+          </div>
+          <div className='HenryContainer'>
+            <p className='HenryName'>Henry</p>
+            <p className='ClickHenry'>kliknij na mnie!</p>
+            <a href={links.henry_link} target="_blank" rel="noreferrer"><img src={henry2} className="Henry"/></a>
+          </div>
+          <div className='InkaContainer'>
+            <p className='InkaName'>Inka</p>
+            <p className='ClickInka'>kliknij na mnie!</p>
+            <a href={links.inka_link} target="_blank" rel="noreferrer"><img src={inka} className="Inka"/></a>
+          </div>
+          <div className='PikotkaContainer'>
+            <p className='PikotkaName'>Pikotka</p>
+            <p className='ClickPikotka'>kliknij na mnie!</p>
+            <a href={links.pikotka_link} target="_blank" rel="noreferrer"><img src={pikotka} className="Pikotka"/></a>
+          </div>
         </div>
+        <div className='Try'>
+          <div className='Within'></div>
+          <div className='Within2'></div>
+        </div>
+        <h1 className='Questionaries'>Zainteresowany?
+        <a href={links.adoption_questionare} target="_blank" rel="noreferrer" className='QuestionaryAdoption'> Ankieta na ADOPCJĘ</a>
+        <a href={links.temp_home_quiestionare} target="_blank" rel="noreferrer" className='QuestionaryTempHome'> A może dasz kotu dom tymczasowy? </a>
+        </h1>
+
       </body>
       
     </div>
--- src/links.js
+++ src/links.js
@@ -3,6 +3,8 @@ const links = {
     henry_link: "https://www.olx.pl/d/oferta/pokotaj-henrysia-cudowny-mlody-miziak-szuka-swoich-kolan-CID103-IDSZ95N.html",
     inka_link: "https://www.olx.pl/d/oferta/pokotaj-inke-przeslodka-kotka-szuka-swojego-domu-CID103-IDTi7rR.html",
     pikotka_link: "https://www.olx.pl/d/oferta/pokotaj-pikotke-mloda-kochana-kicia-szuka-domu-CID103-IDSVt1r.html",
+    adoption_questionare: "https://docs.google.com/forms/d/e/1FAIpQLSe32eMStAmsaVpUmZMAhlmCdg4SxWgNyffnKo-9g3pEkV3APQ/viewform?usp=sharing",
+    temp_home_quiestionare: "https://drive.google.com/file/d/14PPwnZ7qqkopX8T2FLUiDiWcAkX3xkSG/view?usp=sharing", 
 
 }
 export default links
